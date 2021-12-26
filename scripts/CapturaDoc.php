0c0fe828c2fe17ad71a300b317aee220c0c73be4
PlayZera <juninhomacedo2010@hotmail.com>
2021-12-25 22:48:43 -0300

    Ajuste de upload de documento

--- scripts/CapturaDoc.php
+++ scripts/CapturaDoc.php
@@ -1,5 +1,19 @@
-
-
 <?php
+// diretorio utilizado para salvar os arquivos de audio
 
+$dir = "../audios";
+
+//documento do usuário upado
+$audio = $_FILES["mp3_sound"];
+
+$nomeDoDoc =  $audio["name"];
+
+//verifica se o arquivo foi enviado com sucesso
+if (move_uploaded_file($audio["tmp_name"], "$dir/".$audio["name"])) 
+{ 
+    echo "Arquivo enviado com sucesso!"; 
+} 
+else { 
+    echo "Erro, o arquivo n&atilde;o pode ser enviado."; 
+}
 ?>
